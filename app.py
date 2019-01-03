aabbcf78fd37f1974165281b704a7536d61f05b0
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-03 00:57:52 -0800

    imported pymongo created a config of db name and mongo uri stored in db

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'runners_mind_python'
+app.config['MONGO_URI'] = 'mongodb://aj:aj123456@ds052819.mlab.com:52819/runners_mind_python'
+
+db = PyMongo(app)
 
 if __name__ == '__main__':
   app.run(port = 3800, debug=True)
