6ee9f27821d329a1e46d66d644a6e4e075f0f5fd
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-03 02:30:02 -0800

    created crud for user routes

--- routes/user.py
+++ routes/user.py
@@ -1,16 +1,67 @@
 from app import app, mongo
-from flask import jsonify
+from flask_restful import Resource, Api
+from flask import jsonify, request
+from bson.objectid import ObjectId
 
-@app.route('/api/users')
-def get_all_users():
+api = Api(app)
+
+class GetAllUsers(Resource):
+  def get(self):
     users = mongo.db.users
-    result = []
+    data = []
 
     for field in users.find():
-      result.append(
+      data.append(
         {
           '_id': str(field['_id']),
-          'name': field['name']
+          'username': field['name']
         }
       )
-    return jsonify(result)
+    return jsonify(data)
+
+class AddUser(Resource):
+  def post(self):
+    users = mongo.db.users
+    name = request.get_json()['name']
+
+    name_id = users.insert(
+      {
+        'name': name
+      }
+    )
+
+    new_user = users.find_one({'_id': name_id})
+
+    result = {'username': new_user['name'], 'message': 'user was created successfully'}
+
+    return jsonify({'data': result})
+
+class UpdateUser(Resource):
+  def put(self, id):
+    users = mongo.db.users
+    name = request.get_json()['name']
+
+    users.find_one_and_update({'_id': ObjectId(id)}, {'$set': {'name': name}}, upsert=False)
+    new_user = users.find_one({'_id': ObjectId(id)})
+
+    result = {'username': new_user['name'], 'message': 'user was updated successfully'}
+
+    return jsonify({'data': result})
+
+class DeleteUser(Resource):
+  def delete(self, id):
+    users = mongo.db.users
+
+    response = users.delete_one({'_id': ObjectId(id)})
+
+    if response.deleted_count == 1:
+      result = {'message': 'user deleted successfully'}
+    else:
+      result = {'message': 'failed to delete user'}
+
+    return jsonify({'data': result})
+
+api.add_resource(GetAllUsers, '/api/users')
+api.add_resource(AddUser, '/api/users')
+api.add_resource(UpdateUser, '/api/users/<id>')
+api.add_resource(DeleteUser, '/api/users/<id>')
