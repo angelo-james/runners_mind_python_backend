1cc0b07d51bd855950e8e51048f9965a931e9fac
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-03 01:07:12 -0800

    removed mongodb name and mongo uri config from app.py to config.py file

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'runners_mind_python'
-app.config['MONGO_URI'] = 'mongodb://aj:aj123456@ds052819.mlab.com:52819/runners_mind_python'
+app.config.from_pyfile('config.py')
 
 db = PyMongo(app)
 
 if __name__ == '__main__':
-  app.run(port = 3800, debug=True)
+  app.run(port=3800, debug=True)
---
+++ config.py
@@ -0,0 +1,2 @@
+MONGO_DBNAME = 'runners_mind_python'
+MONGO_URI = 'mongodb://aj:aj123456@ds052819.mlab.com:52819/runners_mind_python'
