168d086d7c979d5b4a621e4ef0782064a3ae50fb
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-10 11:45:40 -0800

    implemented auth with bcrypt and jwt

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py')
 
 mongo = PyMongo(app)
+
+app.config['JWT_SECRET_KEY'] = 'secret'
+jwt = JWTManager(app)
 
 from routes.user import *
 from routes.post import *
