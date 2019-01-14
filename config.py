6e82ac6c664c77c91d6b2c8ac4871ab2e38d8130
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-14 13:35:11 -0800

    deploy backend

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-MONGO_DBNAME = 'runners_mind_python'
-MONGO_URI = 'mongodb://aj:aj123456@ds052819.mlab.com:52819/runners_mind_python'
+import os
+
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME') or 'runners_mind_python'
+MONGO_URI = os.environ.get('MONGODB_URI') or 'mongodb://aj:aj123456@ds052819.mlab.com:52819/runners_mind_python'
